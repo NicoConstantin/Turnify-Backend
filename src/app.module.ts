71b1efe959c59e000a6ab1c723e8380353626ae0
Nico <nicoconstantin11@gmail.com>
2024-10-21 20:29:37 +0200

    feat: database,throttler and config to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,37 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { HealthModule } from './health/health.module'
+import configuration from 'config/configuration'
+import { ThrottlerModule } from '@nestjs/throttler'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [configuration] }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.username'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
+        autoLoadEntities: true,
+        synchronize: configService.get<boolean>('database.synchronize'),
+      }),
+    }),
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
